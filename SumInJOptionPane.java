4cb74bdaecfa3cf386e7971b4563c961b2507d90
Happy <89575098+Happy-del3@users.noreply.github.com>
2024-03-28 17:54:28 +0200

    Update SumInJOptionPane.java

--- SumInJOptionPane.java
+++ SumInJOptionPane.java
@@ -1,22 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package suminjoptionpane;
-
 import javax.swing.JOptionPane;
-
-/**
- *
- * @author user
- */
+ 
 public class SumInJOptionPane {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         // Declare variables and constants
         int num1,num2,sum; 
         String strNum1,strNum2;
